f35c6ffab45e35a0ea5a741760ad3b037b743ee9
Daniel Fone <daniel.fone@gmail.com>
2011-11-30 12:11:09 +1300

    Render payments in admin view

--- app/controllers/breeze/pay_online/payments_controller.rb
+++ app/controllers/breeze/pay_online/payments_controller.rb
@@ -2,7 +2,7 @@ module Breeze
   module PayOnline
     class PaymentsController < Breeze::Admin::AdminController
       def index
-        @payments = Breeze::PayOnline::Payment.desc(:created_at).paginate :per_page => 20, :page => params[:page]
+        @payments = Payment.desc(:created_at).paginate :per_page => 20, :page => params[:page]
       end
       
       def show
