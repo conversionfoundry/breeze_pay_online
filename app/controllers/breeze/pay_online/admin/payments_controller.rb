7fd355abae11478f6a1e6a9f6e3bcd699fd6e6c4
Daniel Fone <daniel.fone@gmail.com>
2012-01-16 10:31:52 +1300

    Refactored admin payments controller

--- app/controllers/breeze/pay_online/admin/payments_controller.rb
+++ app/controllers/breeze/pay_online/admin/payments_controller.rb
@@ -1,15 +1,17 @@
 module Breeze
   module PayOnline
-    class Admin::PaymentsController < Breeze::Admin::AdminController
-      def index
-        @payments = Payment.desc(:created_at).paginate :per_page => 20, :page => params[:page]
-      end
+    module Admin
+      class PaymentsController < Breeze::Admin::AdminController
+        def index
+          @payments = Payment.desc(:created_at).paginate :per_page => 20, :page => params[:page]
+        end
       
-      def show
-        @payment = Breeze::PayOnline::Payment.find params[:id]
-        render :layout => "enquiry"
-      end
+        def show
+          @payment = Breeze::PayOnline::Payment.find params[:id]
+          render :layout => "enquiry"
+        end
       
+      end
     end
   end
 end
