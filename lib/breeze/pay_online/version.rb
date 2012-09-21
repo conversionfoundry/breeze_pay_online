f615756dd19567d89f6dfb74d0bd64999e57875b
Isaac Freeman <isaac@leftclick.com>
2012-09-21 14:55:03 +1200

    version.rb modified to match other engines

--- lib/breeze/pay_online/version.rb
+++ lib/breeze/pay_online/version.rb
@@ -1,6 +1,10 @@
 module Breeze
   module PayOnline
-    VERSION = "0.0.1"
-  end
+		MAJOR = 0		# increment for new backwards incompatible changes
+	  MINOR = 0		# increment for new backward-compatible functionality
+	  PATCH = 1		# increment for backwards-compaitble bug fixes
+	  PRE = nil # (nil|pre|alpha|a|beta) or every alphanumeric tag you'd like to append to your version.
+
+	  VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join(".")   end
 end
 
